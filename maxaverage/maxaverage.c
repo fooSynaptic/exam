4819ec299118f0fd37b73b0367bda91381d193bf
ordinar <ordinar@sjtu.edu.cn>
2018-06-21 10:20:09 +0800

    fix the len function

--- maxaverage/maxaverage.c
+++ maxaverage/maxaverage.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 
 
-double findMaxAverage(int nums[6], int k) {
+
+double findMaxAverage(int nums[5], int length, int k) {
     // Write your code here
     int s = 0;
     int f = s + k;
-    float max = 0.;
+    double max = 0.;
     
     for(int i=0; i<f;i++)
         max = max + nums[i];
         
-    float maxaver = max/k;
+    double maxaver = max/k;
     printf("current max is %f average is %f\n", max, maxaver);
-    for(int i=f; i<6; i++){
+    for(int i=f; i<length; i++){
         if(nums[i]>maxaver && nums[i]>nums[s]){
             s++;
             max = max + nums[i]-nums[s-1];
@@ -25,10 +26,17 @@ double findMaxAverage(int nums[6], int k) {
 }
 
 
-int main(void){
-	int nums[6] = {1,12,-5,-6,50,3};
-	int k =4;
 
-	printf("%f\n", findMaxAverage(nums,k));
+
+
+int main(void){
+	//int nums[6] = {1,12,-5,-6,50,3};
+	int nums[5] = {3,3,4,3,0};
+	int l = *(&nums+1) - nums;
+	printf("the len of array is %d\n" , l);
+	int k = 3;
+	//int *nump = &nums[0];
+
+	printf("%f\n", findMaxAverage(nums,l,k));
 
 }
