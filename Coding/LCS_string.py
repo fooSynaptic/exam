6e180c8bd199c8b437efb4eecd637188056f832d
fooSynaptic <2313990450@qq.com>
2020-01-26 15:22:33 +0800

    Update LCS_string.py

--- Coding/LCS_string.py
+++ Coding/LCS_string.py
@@ -5,8 +5,8 @@ def lcs_string(s1, s2):
     l1, l2 = len(s1), len(s2)
     dp = [['' for _ in range(l2+1)] for _ in range(l1+1)]
     string = ''
-    for i in range(1, l1):
-        for j in range(1, l2):
+    for i in range(1, l1+1):
+        for j in range(1, l2+1):
             dp[i][j] = max(dp[i-1][j], dp[i][j-1])
             if s1[i-1] == s2[j-1]:
                 dp[i][j] = max(dp[i][j], dp[i-1][j-1]+s1[i-1])
